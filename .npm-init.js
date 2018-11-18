43642caaac640cc9fa15d80e0afb12f6a120b2a6
Federico Ramos <federico.ramos@gmail.com>
2018-11-18 16:05:34 -0300

    First commit on saintsapp

---
+++ .npm-init.js
@@ -0,0 +1,6 @@
+module.exports = propmt("¿Cuál es su sabor de helado favorito?","Me gustan todos");
+
+module.exports = {
+customField: 'Ejemplo de campo personalizado',
+otherCustomField: 'Este ejemplo está muy bueno'
+}
